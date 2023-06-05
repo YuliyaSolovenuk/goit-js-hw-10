da89a3c23da0b1f01f83b0f32e8fb066973ab5b1
YuliyaSolovenuk <121509951+YuliyaSolovenuk@users.noreply.github.com>
2023-06-05 11:22:23 +0000

    Deploying to gh-pages from @ YuliyaSolovenuk/goit-js-hw-10@7a8b0999815db5f866fe776ed3af9cc62dc5d32b 🚀

--- index.047d8426.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".breed-select");console.log(e);var o=document.querySelector(".breed-select");console.log(o)}();
-//# sourceMappingURL=index.047d8426.js.map
--- index.ecd9c624.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".breed-select");console.log(e);const o=document.querySelector(".breed-select");console.log(o);
-//# sourceMappingURL=index.ecd9c624.js.map
